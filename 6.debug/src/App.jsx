4868043bd93c66c92b7960beb8428715cf99b326
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-05-07 15:09:42 +0300

    react

--- 6.debug/src/App.jsx
+++ 6.debug/src/App.jsx
@@ -3,9 +3,40 @@ import './App.css'
 
 function App() {
 
+  const [vize1, setVize1] = useState(0);
+  const [vize2, setVize2] = useState(0);
+
+
+  const ortalamaBul = () => {
+    const toplamSonuc = topla() / 2;
+    yazdir(toplamSonuc);
+  }
+
+  const topla = () => {
+    const toplam = vize1 + vize2
+    return toplam;
+  }
+
+  const yazdir = (sonuc) => {
+    console.log("ortalama : " + sonuc)
+  }
+
   return (
     <>
-     
+      <div>
+        <input type='number' value={vize1} onChange={(e) => setVize1(Number(e.target.value))} />
+      </div>
+
+      <br />
+
+      <div>
+        <input type='number' value={vize2} onChange={(e) => setVize2(Number(e.target.value))} />
+      </div>
+
+
+      <div>
+        <button onClick={ortalamaBul}>ortalama bul</button>
+      </div>
     </>
   )
 }
