260c783d92106a1eb27b62c856bb36731822dae1
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-05-07 14:41:29 +0300

    react

--- 5.useeffect/src/App.jsx
+++ 5.useeffect/src/App.jsx
@@ -1,12 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
 
+const [firstName, setFirstName] = useState("");
+const [lastName, setLastName] = useState("");
+
+
+  useEffect(() => {
+    console.log("her zaman çalışır")
+  })
+
+   useEffect(() => {
+     console.log("ilk render edildiğinde çalışır")
+   },[])
+
+   useEffect(() => {
+    console.log("ilk render edildiğinde ve firstname state değeri değiştiğinde çalışır")
+   }, [firstName])
+
+   useEffect(() => {
+      console.log("ilk render edildiğinde ve lastname state değeri değiştiğinde çalışır")
+   }, [lastName])
+
+   useEffect(() => {
+    console.log("ilk render edildiğinde ve firstname veya lastname state değerlerinden değişiklik olduğunda çalışır")
+   },[firstName, lastName])
+
   return (
-    <>
-     
-    </>
+    <div>
+    <div>
+    <button onClick={() => setFirstName("nesliş")}>adı değiştir</button>
+    </div>
+
+    <div>
+    <button onClick={() => setLastName("ulupınar")}>soyadı değiştir</button>
+    </div>
+    </div>
   )
 }
 
