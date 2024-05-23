02cf09fe94c90240e10c01181e0045563f7ba909
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-05-23 16:49:01 +0300

    react

--- doviz-kuru-uygulaması/src/components/Currency.jsx
+++ doviz-kuru-uygulaması/src/components/Currency.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { Children, useState } from 'react'
 import "../css/currency.css";
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
 
 function Currency() {
+
+   const [amount, setAmount] = useState(0);
+   const [fromCurrency, setFromCurrency] = useState("");
+   const [toCurrency, setToCurrency] = useState("");
+   const [result, setResult] = useState(0);
+
+   const exchange = () => {
+    console.log(amount)
+    console.log(fromCurrency)
+    console.log(toCurrency)
+   }
+
   return (
     <div className='currency-div'>
       <div>
@@ -10,8 +22,8 @@ function Currency() {
       </div>
 
       <div>
-        <input type="number" className="amount" />
-        <select className='from-currency-option'>
+        <input value={amount} onChange={(e) => setAmount(e.target.value)} type="number" className="amount" />
+        <select onChange={(e) => setFromCurrency(e.target.value)} className='from-currency-option'>
           <option>USD</option>
           <option>EUR</option>
           <option>TL</option>
@@ -19,9 +31,9 @@ function Currency() {
 
         <FaRegArrowAltCircleRight style={{ fontSize: "25px", marginRight: "10px", color: "black" }} />
 
-        <input type="number" className="result" />
+        <input value={result} onChange={(e) => setResult(e.target.value)} type="number" className="result" />
 
-        <select className='to-currency-option'>
+        <select onChange={(e) => setToCurrency(e.target.value)} className='to-currency-option'>
           <option>TL</option>
           <option>USD</option>
           <option>EUR</option>
@@ -30,7 +42,8 @@ function Currency() {
         
       </div>
       <div>
-        <button className='exchange-button'>Çevir</button>
+        <button onClick={exchange}
+        className='exchange-button'>Çevir</button>
       </div>
     </div>
   )
