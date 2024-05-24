51b1c890df2e7f27cebe9aee5524a7aeabd20900
neslihan ulupınar <neslihanulpnr0@hotmail.com>
2024-05-24 12:09:58 +0300

    react

--- doviz-kuru-uygulaması/src/components/Currency.jsx
+++ doviz-kuru-uygulaması/src/components/Currency.jsx
@@ -1,6 +1,7 @@
 import React, { Children, useState } from 'react'
 import "../css/currency.css";
 import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import axios from "axios"
 
 let BASE_URL = "https://api.freecurrencyapi.com/v1/latest"
 let API_KEY = "fca_live_XURFCn0c1S9MvrDj8UceJgHjYAJfBsFPyFEFeBXE"
@@ -8,14 +9,17 @@ let API_KEY = "fca_live_XURFCn0c1S9MvrDj8UceJgHjYAJfBsFPyFEFeBXE"
 function Currency() {
 
    const [amount, setAmount] = useState(0);
-   const [fromCurrency, setFromCurrency] = useState("");
-   const [toCurrency, setToCurrency] = useState("");
+   const [fromCurrency, setFromCurrency] = useState("USD");
+   const [toCurrency, setToCurrency] = useState("TL");
    const [result, setResult] = useState(0);
 
-   const exchange = () => {
-    console.log(amount)
-    console.log(fromCurrency)
-    console.log(toCurrency)
+   const exchange = async () => {
+    //console.log(amount)
+    //console.log(fromCurrency)
+    //console.log(toCurrency)
+    
+  const response = await axios.get(`${BASE_URL}?apikey=${API_KEY}&base_currency=${fromCurrency}`)
+  console.log(response);
    }
 
   return (
